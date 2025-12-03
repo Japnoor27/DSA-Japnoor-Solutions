2102fe13dfa3acc5142605810afc5823d988ac73
Japnoor27 <japnoorkd@gmail.com>
2025-12-03 10:49:59 +0530

    Time: 1 ms (64.58%), Space: 46.9 MB (7.59%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+        int j=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=nums[j-1]){
+                nums[j++]=nums[i];
+            }
+        }
+        return j;
+    }
+}
