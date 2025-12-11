fac69c4b20ed32435a44f01ffa166d7ea878a504
Japnoor27 <japnoorkd@gmail.com>
2025-12-11 18:05:14 +0530

    Time: 3 ms (78.02%), Space: 43.3 MB (35.04%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> st=new Stack <>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                char c=st.pop();
+                if(c=='(' && ch==')') continue;
+                if(c== '[' && ch==']') continue;
+                if(c== '{' && ch=='}') continue;
+                return false;
+            }
+        }
+        return st.empty();
+    }
+}
