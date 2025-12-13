2acd3582b1f761d226d6935f4a6e25079b319778
Japnoor27 <japnoorkd@gmail.com>
2025-12-13 15:07:10 +0530

    Time: 1 ms (93.79%), Space: 42.7 MB (21.78%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,22 +1,24 @@
 class Solution {
     public int mySqrt(int x) {
-        int s = 0;
-        int e = x;
-        int found = -1;
+
+        int s = 0, e = x;
+        int ans = -1;
 
         while (s <= e) {
             int mid = s + (e - s) / 2;
-            long val = (long) mid * mid;  // prevent overflow
+            long sq = (long) mid * mid;  // ✅ FIX
 
-            if (val == x) {
+            if (sq == x) {
                 return mid;
-            } else if (val < x) {
-                found = mid;   // candidate
+            } 
+            else if (sq < x) {
+                ans = mid;
                 s = mid + 1;
-            } else {
+            } 
+            else {
                 e = mid - 1;
             }
         }
-        return found;  // floor(sqrt(x))
+        return ans;
     }
 }
