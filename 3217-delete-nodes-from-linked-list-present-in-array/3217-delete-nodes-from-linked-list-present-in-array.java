b077f01f5125cfc2bc3bf03cfa2f0346be032ad2
Japnoor27 <japnoorkd@gmail.com>
2026-01-22 13:58:06 +0530

    Time: 10 ms (92.45%), Space: 157.9 MB (61.9%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
@@ -1,28 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-
-        int MAX = 100000;   // value range
-        int[] freq = new int[MAX + 1];
-
-        // nums wale mark kar do
-        for (int x : nums) {
-            freq[x] = 1;
+        ListNode temp=head;
+        ListNode dummy=new ListNode(-1);
+        ListNode tail=dummy;
+        int[] freq=new int[100001];
+    while(temp!=null){
+        freq[temp.val]=1;
+        temp=temp.next;
+    }
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]]=0;
         }
+        temp=head;
+        while(temp!=null){
+            if(freq[temp.val]==1){
+                tail.next=new ListNode(temp.val);
+                tail=tail.next;
 
-        ListNode dummy = new ListNode(-1);
-        ListNode tail = dummy;
-        ListNode temp = head;
-
-        while (temp != null) {
-            // agar nums mein nahi hai → rakh lo
-            if (freq[temp.val] == 0) {
-                tail.next = new ListNode(temp.val);
-                tail = tail.next;
             }
-            // agar nums mein hai → skip (delete)
-            temp = temp.next;
+            temp=temp.next;
         }
 
-        return dummy.next;
+return dummy.next;
     }
 }
