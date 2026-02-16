f1a0eaa4e60991f9edaabed84d98dad7383a62e1
Japnoor27 <japnoorkd@gmail.com>
2026-02-17 01:36:38 +0530

    Time: 3 ms (54.77%), Space: 45.1 MB (61.42%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public void find(int index,int nums[],List<Integer> ds,List<List<Integer>> a){
-        a.add(new ArrayList<>(ds));
+        a.add(new ArrayList<>(ds)); //not to add base case Subsets / SubsetsWithDup me har recursion call khud ek valid subset hota hai, isliye answer start me add hota hai; index == length ka base case lagane se beech ke subsets miss ho jaate hain.
         for(int i=index;i<nums.length;i++){
             if(i!=index && nums[i]==nums[i-1]) continue;
             ds.add(nums[i]);
