829dcca6c5ed50f90adc9c2f06e7305c4b733c6c
Japnoor27 <japnoorkd@gmail.com>
2026-02-19 00:58:06 +0530

    Time: 1 ms (99.74%), Space: 46.6 MB (37.05%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,35 +1,42 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode temp1=l1;
-        ListNode temp2=l2;
-        ListNode dummy=new ListNode(-1);
-        ListNode curr=dummy;
-        int carry=0;
-        while(temp1!=null ||temp2!=null || carry!=0){
-            int sum=carry;
-            if(temp1!=null){
-                sum+=temp1.val;
-                temp1=temp1.next;
-            }
-            if(temp2!=null){
-                sum+=temp2.val;
-                temp2=temp2.next;
+    int carry = 0;   // class-level carry
 
-            }
-            carry=sum/10;
-            curr.next=new ListNode(sum%10);
-            curr=curr.next;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        // base case
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
         }
-        return dummy.next;
+
+        int val1 = 0;
+        int val2 = 0;
+
+        if (l1 != null) {
+            val1 = l1.val;
+        }
+
+        if (l2 != null) {
+            val2 = l2.val;
+        }
+
+        int sum = val1 + val2 + carry;
+        carry = sum / 10;
+
+        ListNode ans = new ListNode(sum % 10);
+
+        ListNode next1 = null;
+        ListNode next2 = null;
+
+        if (l1 != null) {
+            next1 = l1.next;
+        }
+
+        if (l2 != null) {
+            next2 = l2.next;
+        }
+
+        ans.next = addTwoNumbers(next1, next2);
+
+        return ans;
     }
 }
