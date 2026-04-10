ea48ea42948d6dc0019711d83ba70bae929b1b36
Japnoor27 <japnoorkd@gmail.com>
2026-04-10 16:45:18 +0530

    Time: 0 ms (100%), Space: 44.2 MB (79.69%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -17,24 +17,20 @@ class Solution {
         while(temp1!=null && temp2!=null){
             if(temp1.val<=temp2.val){
                 curr.next=temp1;
-               
                 temp1=temp1.next;
             }
             else{
                 curr.next=temp2;
-               
-                temp2=temp2.next;
+               temp2=temp2.next;
             }
             curr=curr.next;
         }
         if(temp1!=null){
             curr.next=temp1;
-            
-        }
+         }
         else{
              curr.next=temp2;
-            
-        }
+            }
         return dummy.next;
     }
 }
