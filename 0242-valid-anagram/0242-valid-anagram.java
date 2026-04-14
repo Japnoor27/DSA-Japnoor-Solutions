b4e5079cb44b6f0881cccbf170213babac7566dc
Japnoor27 <japnoorkd@gmail.com>
2026-04-14 18:35:37 +0530

    Time: 17 ms (16.55%), Space: 46.6 MB (18.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,26 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() !=t.length()){
-            return false;
-        }
-        int[] freq=new int[26];
+        Map <Character,Integer> mp=new HashMap <>();
+        Map <Character,Integer> mp2=new HashMap <>();
+        if(s.length()!=t.length()) return false;
         for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
+            Character ch=s.charAt(i);
+           if(mp.containsKey(ch)){
+            mp.put(ch,mp.get(ch)+1);
+           }
+           else{
+            mp.put(ch,1);
+           }
         }
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]!=0){
-                return false;
-            }
+        for(int i=0;i<t.length();i++){
+          Character ch=t.charAt(i);
+           if(mp2.containsKey(ch)){
+            mp2.put(ch,mp2.get(ch)+1);
+           }
+           else{
+            mp2.put(ch,1);
+           }
         }
-        return true;
+        return mp.equals(mp2);
     }
 }
