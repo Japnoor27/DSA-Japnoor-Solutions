e68364bb3c65b3e1623dc0a017ce0fe60fe0c05b
Japnoor27 <japnoorkd@gmail.com>
2026-04-14 18:39:37 +0530

    Time: 18 ms (13.44%), Space: 46.5 MB (35.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,7 +1,28 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map <Character,Integer> mp=new HashMap <>();
-        Map <Character,Integer> mp2=new HashMap <>();
+        // Map <Character,Integer> mp=new HashMap <>();
+        // Map <Character,Integer> mp2=new HashMap <>();
+        // if(s.length()!=t.length()) return false;
+        // for(int i=0;i<s.length();i++){
+        //     Character ch=s.charAt(i);
+        //    if(mp.containsKey(ch)){
+        //     mp.put(ch,mp.get(ch)+1);
+        //    }
+        //    else{
+        //     mp.put(ch,1);
+        //    }
+        // }
+        // for(int i=0;i<t.length();i++){
+        //   Character ch=t.charAt(i);
+        //    if(mp2.containsKey(ch)){
+        //     mp2.put(ch,mp2.get(ch)+1);
+        //    }
+        //    else{
+        //     mp2.put(ch,1);
+        //    }
+        // }
+        // return mp.equals(mp2);
+        Map <Character,Integer> mp=new HashMap<>();
         if(s.length()!=t.length()) return false;
         for(int i=0;i<s.length();i++){
             Character ch=s.charAt(i);
@@ -13,14 +34,19 @@ class Solution {
            }
         }
         for(int i=0;i<t.length();i++){
-          Character ch=t.charAt(i);
-           if(mp2.containsKey(ch)){
-            mp2.put(ch,mp2.get(ch)+1);
+            Character ch=t.charAt(i);
+           if(mp.containsKey(ch)){
+            mp.put(ch,mp.get(ch)-1);
            }
            else{
-            mp2.put(ch,1);
+            mp.put(ch,1);
            }
         }
-        return mp.equals(mp2);
+        for(var e:mp.values()){
+            if(e!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
