88d330d105559f8005807c7ac058624f4b3c6e33
Japnoor27 <japnoorkd@gmail.com>
2026-04-16 18:28:37 +0530

    Time: 3 ms (56.65%), Space: 44.8 MB (90.12%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,19 +1,20 @@
 class Solution {
-    public void find(int index,int nums[],List<Integer> ds,List<List<Integer>> a){
-        a.add(new ArrayList<>(ds)); //not to add base case Subsets / SubsetsWithDup me har recursion call khud ek valid subset hota hai, isliye answer start me add hota hai; index == length ka base case lagane se beech ke subsets miss ho jaate hain.
-        for(int i=index;i<nums.length;i++){
-            if(i!=index && nums[i]==nums[i-1]) continue;
-            ds.add(nums[i]);
-            find(i+1,nums,ds,a);
-            ds.remove(ds.size()-1);
-
+    public void backtrack(int ind,int[] nums, List<List<Integer>> ans,List<Integer> temp){
+        ans.add(new ArrayList <>(temp));
+        for(int i=ind;i<nums.length;i++){
+            if(i!=ind && nums[i]==nums[i-1]){
+             continue;
+            }
+               temp.add(nums[i]);
+                backtrack(i+1,nums,ans,temp);
+                temp.remove(temp.size()-1);
         }
-        }
+    }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> a=new ArrayList<>();
-        List <Integer> ds=new ArrayList<>();
-        find(0,nums,ds,a);
-        return a;
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> temp=new ArrayList <>();
+        backtrack(0,nums,ans,temp);
+        return ans;
     }
 }
