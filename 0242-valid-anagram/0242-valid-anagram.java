7ee81ac5f10964dbe7cc7f172bd5599d424830ab
Japnoor27 <japnoorkd@gmail.com>
2026-07-01 17:32:30 +0530

    Time: 16 ms (21.15%), Space: 46.6 MB (16.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,49 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // Map <Character,Integer> mp=new HashMap <>();
-        // Map <Character,Integer> mp2=new HashMap <>();
-        // if(s.length()!=t.length()) return false;
-        // for(int i=0;i<s.length();i++){
-        //     Character ch=s.charAt(i);
-        //    if(mp.containsKey(ch)){
-        //     mp.put(ch,mp.get(ch)+1);
-        //    }
-        //    else{
-        //     mp.put(ch,1);
-        //    }
-        // }
-        // for(int i=0;i<t.length();i++){
-        //   Character ch=t.charAt(i);
-        //    if(mp2.containsKey(ch)){
-        //     mp2.put(ch,mp2.get(ch)+1);
-        //    }
-        //    else{
-        //     mp2.put(ch,1);
-        //    }
-        // }
-        // return mp.equals(mp2);
-        Map <Character,Integer> mp=new HashMap<>();
-        if(s.length()!=t.length()) return false;
-        for(int i=0;i<s.length();i++){
-            Character ch=s.charAt(i);
-           if(mp.containsKey(ch)){
-            mp.put(ch,mp.get(ch)+1);
-           }
-           else{
-            mp.put(ch,1);
-           }
+        HashMap <Character,Integer> map=new HashMap <>();
+        for(char ch:s.toCharArray()){
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+            }
+            else{
+                map.put(ch,1);
+            }
         }
-        for(int i=0;i<t.length();i++){
-            Character ch=t.charAt(i);
-           if(mp.containsKey(ch)){
-            mp.put(ch,mp.get(ch)-1);
-           }
-           else{
-            mp.put(ch,1);
-           }
+        for(char ch:t.toCharArray()){
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)-1);
+            }
+            else{
+                return false;
+            }
+            
         }
-        for(var e:mp.values()){
-            if(e!=0){
+        for(char ch:map.keySet()){
+            if(map.get(ch)!=0){
                 return false;
             }
         }
