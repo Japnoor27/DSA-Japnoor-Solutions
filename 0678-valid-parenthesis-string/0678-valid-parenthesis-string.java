7ebd5c0597e8f57053fd7f610f23ad135a145987
Japnoor27 <japnoorkd@gmail.com>
2026-07-01 13:03:48 +0530

    Time: 0 ms (100%), Space: 43 MB (18.68%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int low=0,high=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='('){
+                low++;
+                high++;
+            }
+            else if(ch==')'){
+                low--;
+                high--;
+            }
+            else{
+                low--;
+                high++;
+            }
+            low=Math.max(low,0);
+            if(high<0) return false;
+        }
+        return low==0;
+    }
+}
