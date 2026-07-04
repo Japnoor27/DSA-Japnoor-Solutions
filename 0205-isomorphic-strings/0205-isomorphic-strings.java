0fc5c049225517192de1fbb1caf67d871334a341
Japnoor27 <japnoorkd@gmail.com>
2026-07-04 14:07:33 +0530

    Time: 10 ms (71.93%), Space: 43.9 MB (42.28%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,36 +1,34 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-    //     if(s.length()!=t.length()) return false;
-    //     Map <Character,Character> mp=new HashMap<>();
-    //     for(int i=0;i<s.length();i++){
-    //         Character sCh=s.charAt(i);
-    //         Character tCh=t.charAt(i);
-    //         if(mp.containsKey(sCh) ){
-    //             if(mp.get(sCh)!=tCh){
-    // return false;}
-    //              }
-            
-    //         else{
-    //             if(mp.containsValue(tCh)) return false;
-    //     mp.put(sCh,tCh);
-    //         }}
-    //     return true;
-    if(s.length()!=t.length()) return false;
-    HashSet <Character> st=new HashSet <>();
-        Map <Character,Character> mp=new HashMap<>();
+        if(s.length()!=t.length()) return false;
+        HashMap <Character,Character> map=new HashMap <>();
+         HashMap <Character,Character> map2=new HashMap <>();
         for(int i=0;i<s.length();i++){
-            Character sCh=s.charAt(i);
-            Character tCh=t.charAt(i);
-            if(mp.containsKey(sCh) ){
-                if(mp.get(sCh)!=tCh){
-    return false;}
-                 }
+            char ch1=s.charAt(i);
+            char ch2=t.charAt(i);
+                if(map.containsKey(ch1)){
+                    if(ch2!=map.get(ch1)){
+                    return false;}
+                    
+                }
+                else{
+                    map.put(ch1,ch2);
+
+                }
+            }
+            for(int i=0;i<s.length();i++){
+                char ch1=s.charAt(i);
+                char ch2=t.charAt(i);
+                if(map2.containsKey(ch2)){
+                    if(ch1!=map2.get(ch2)){
+                        return false;
+                    }
+                }
+                else{
+                    map2.put(ch2,ch1);
+                }
             
-            else{
-                if(st.contains(tCh)) return false;
-        mp.put(sCh,tCh);
-        st.add(tCh);
-            }}
+        }
         return true;
     }
 }
