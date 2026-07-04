7c5759bff1e7f732a32ade71d0d79e961e93af01
Japnoor27 <japnoorkd@gmail.com>
2026-07-04 15:08:26 +0530

    Time: 0 ms (100%), Space: 42.8 MB (71.19%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count=0;
+        int max=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(')
+             {
+                count++;
+                max=Math.max(count,max);
+
+                }
+                else if(s.charAt(i)==')'){
+                    if(count>0) count--;
+                }
+        }
+        return max;
+    }
+}
