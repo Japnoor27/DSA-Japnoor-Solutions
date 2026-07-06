22e64da6083a6ff7f93a8463f72a7d8addf50f85
Japnoor27 <japnoorkd@gmail.com>
2026-07-06 11:15:01 +0530

    Time: 9 ms (17.23%), Space: 43.2 MB (42.2%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -2,13 +2,14 @@ class Solution {
     public int longestPalindrome(String s) {
         HashMap <Character,Integer> map=new HashMap<>();
         for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
-            }
-            else{
-                map.put(ch,1);
-            }
+            // char ch=s.charAt(i);
+            // if(map.containsKey(ch)){
+            //     map.put(ch,map.get(ch)+1);
+            // }
+            // else{
+            //     map.put(ch,1);
+            // }
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
         }
         boolean flag=false;
         int count=0;
