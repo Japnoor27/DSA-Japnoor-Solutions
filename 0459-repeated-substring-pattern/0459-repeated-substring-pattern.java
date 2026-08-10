c8f9306a3f69a9ce5ad5ce45cdb67ad8de56efe1
Japnoor27 <japnoorkd@gmail.com>
2026-08-10 11:45:03 +0530

    Time: 6 ms (99.08%), Space: 47 MB (20.62%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        int n=s.length();
         int i=1;
+        int n=s.length();
         int len=0;
-        int[]lps=new int[n];
-        while(i<s.length()){
+        int[] lps=new int[n];
+        while(i<n){
             if(s.charAt(i)==s.charAt(len)){
                 len++;
                 lps[i]=len;
@@ -19,7 +19,6 @@ class Solution {
                     i++;
                 }
             }
-
         }
         int check=n-lps[n-1];
         if(n%check==0 && lps[n-1]!=0) return true;
