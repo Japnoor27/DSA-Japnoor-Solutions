c6407d86e48ff8e7a9077ff22b2d6ac785b8dac6
Japnoor27 <japnoorkd@gmail.com>
2026-08-11 17:39:24 +0530

    Time: 4 ms (92.8%), Space: 44.4 MB (72.11%) - LeetHub

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
@@ -5,19 +5,15 @@ class Solution {
             sum_apple+=num;
         }
         Arrays.sort(capacity);
-       for(int i = 0; i < capacity.length / 2; i++){
-    int temp = capacity[i];
-    capacity[i] = capacity[capacity.length - 1 - i];
-    capacity[capacity.length - 1 - i] = temp;
-}
+   
         int count=0;
 
-        for(int num:capacity){
+        for(int i=capacity.length-1;i>=0;i--){
             if(sum_apple<=0){
                break;
             }
              count++;
-                sum_apple-=num;
+                sum_apple-=capacity[i];
 
         }
         return count;
