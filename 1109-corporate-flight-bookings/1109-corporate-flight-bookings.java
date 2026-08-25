8ab40ee28007518d507bdd9d52656581dd5364a7
Japnoor27 <japnoorkd@gmail.com>
2026-08-25 17:17:26 +0530

    Time: 3 ms (98.38%), Space: 84.8 MB (49.55%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -1,14 +1,33 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int[] diff=new int[n];
+        // int[] diff=new int[n];
+        // for(int[] b:bookings){
+        //     int start=b[0];
+        //     int end=b[1];
+        //     int add=b[2];
+        //     for(int i=start-1;i<end;i++){
+        //         diff[i]+=add;
+        //     }
+        // }
+        // return diff;
+        int[] diff=new int[n+1];
         for(int[] b:bookings){
             int start=b[0];
             int end=b[1];
             int add=b[2];
-            for(int i=start-1;i<end;i++){
-                diff[i]+=add;
-            }
+           
+                diff[start-1]+=add;
+                diff[end]-=add;
+
         }
-        return diff;
+            int cur=0;
+            int[] ans=new int[n];
+            for(int i=0;i<n;i++){
+                cur+=diff[i];
+                ans[i]=cur;
+            }
+        
+        return ans;
     }
+
 }
