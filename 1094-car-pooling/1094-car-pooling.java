6c3284cbc4137a7fa629871e4ca82c50f3a99b69
Japnoor27 <japnoorkd@gmail.com>
2026-08-25 16:56:39 +0530

    Time: 2 ms (60.17%), Space: 46.5 MB (24.79%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+       int[] diff=new int[1001];
+       for(int[] trip:trips){
+        int passengers=trip[0];
+        int from=trip[1];
+        int to=trip[2];
+        diff[from]+=passengers;
+        diff[to]-=passengers;
+       } 
+       int curr=0;
+       for(int i=0;i<diff.length;i++){
+        curr+=diff[i];
+        if(curr>capacity){
+            return false;
+        }
+        
+       }
+       return true;
+    }
+}
