4d4816f4bc4bdaf8d0dd7e7105868d213ce4f8ec
Japnoor27 <japnoorkd@gmail.com>
2026-08-27 00:20:47 +0530

    Time: 24 ms (76.24%), Space: 48.9 MB (47.3%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,36 +1,17 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-    HashMap <Integer,Integer> map=new HashMap <>();
-    map.put(0,1);
-    int sum=0;
-    int count=0;
-    for(int num:nums){
-        sum+=num;
-        int diff=sum-k;
-        if(map.containsKey(diff)){
-            count+=map.get(diff);
+        int sum=0,count=0;
+        HashMap <Integer,Integer> map=new HashMap <>();
+        map.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            int diff=sum-k;
+            if(map.containsKey(diff)){
+                count+=map.get(diff);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
-        map.put(sum,map.getOrDefault(sum,0)+1);
-    }
-    return count;
+        return count;
+
     }
 }
-
-
-// class Solution {
-//     public int atMost(int[] nums,int k){
-//         int left=0,count=0,sum=0;
-//         for(int i=0;i<nums.length;i++){
-//         sum+=nums[i];
-//         if(sum>k){
-//             sum-=nums[left];
-//             left++;
-
-//         }
-//         count+=i-left+1;}
-//         return count;
-//     }
-//     public int subarraySum(int[] nums, int k) {
-// return atMost(nums,k)-atMost(nums,k-1);
-//     }
-// }
