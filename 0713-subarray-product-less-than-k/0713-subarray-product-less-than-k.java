55cc906addd3697e584cf519a7a89542f488a356
Japnoor27 <japnoorkd@gmail.com>
2026-08-27 11:20:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,15 +1,14 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int left=0,count=0,product=1;
+        int count=0,prod=1,left=0;
         for(int i=0;i<nums.length;i++){
-            product*=nums[i];
-            while(product>=k){
-                product=product/nums[left];
+            prod*=nums[i];
+            while(prod>=k){
+                prod=prod/nums[left];
                 left++;
+
             }
-            if(product<k){
-                count=count+i-left+1;
-            }
+            if(prod<k) count+=i-left+1;
         }
         return count;
     }
