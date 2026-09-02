75200fba95e08d5b9ea7c5d17da40c77bd136a09
Japnoor27 <japnoorkd@gmail.com>
2026-09-02 16:02:23 +0530

    Time: 19 ms (16.38%), Space: 51.2 MB (5.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap <String,List<String> > map=new HashMap<>();
+        HashMap <String,List<String>> map=new HashMap <>();
         for(String str:strs){
             int[] freq=new int[26];
             for(char ch:str.toCharArray()){
@@ -11,7 +11,7 @@ class Solution {
                 map.get(key).add(str);
             }
             else{
-                List<String> list=new ArrayList<>();
+                List <String> list=new ArrayList<>();
                 list.add(str);
                 map.put(key,list);
             }
