0b7a6b9ed47855e0334337f3b81425797cb8675e
Japnoor27 <japnoorkd@gmail.com>
2026-09-02 16:53:53 +0530

    Time: 0 ms (100%), Space: 47.7 MB (67.58%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,9 +1,9 @@
 class Solution {
     public int trap(int[] height) {
-        int left=0,right=height.length-1,leftmax=0,rightmax=0,total=0;
+        int left=0,right=height.length-1,total=0,leftmax=0,rightmax=0;
         while(left<right){
             if(height[left]<height[right]){
-                if(height[left]>leftmax){
+                if(height[left]>=leftmax){
                     leftmax=height[left];
                 }
                 else{
@@ -12,14 +12,13 @@ class Solution {
                 left++;
             }
             else{
-                if(height[right]>rightmax){
+                if(height[right]>=rightmax){
                     rightmax=height[right];
                 }
-                else{
-                    total+=rightmax-height[right];
-                }
-                right--;
-
+            else{
+                total+=rightmax-height[right];
+            }
+            right--;
             }
         }
         return total;
