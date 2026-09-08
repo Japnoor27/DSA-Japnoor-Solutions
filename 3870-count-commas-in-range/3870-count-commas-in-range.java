4be381e5093738e1a8909e672458a9c689412586
Japnoor27 <japnoorkd@gmail.com>
2026-09-08 10:43:04 +0530

    Time: 310 ms (5.4%), Space: 46.6 MB (8.32%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countCommas(int n) {
+        
+        int ans = 0;
+        for(int j=1;j<=n;j++){
+        String str = "";
+        str += Integer.toString(j);
+        if (str.length() < 4)
+            ans+=0;
+        else {
+            int count = 0;
+            for (int i = str.length() - 1; i > 0; i--) {
+
+                count++;
+                if (count == 3) {
+
+                    ans++;
+                    count = 0;
+                }
+            }
+        }}
+        return ans;
+    }
+}
