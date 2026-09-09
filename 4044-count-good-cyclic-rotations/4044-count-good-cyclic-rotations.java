1e3a5ded98bc57778082759dfb8bae763223cf74
Japnoor27 <japnoorkd@gmail.com>
2026-09-09 13:14:11 +0530

    Time: 3 ms (95.53%), Space: 65.2 MB (93.74%) - LeetHub

--- 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.java
+++ 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.java
@@ -2,21 +2,21 @@ class Solution {
     public int countGoodRotations(int[] nums) {
         int n=nums.length;
         int half=n/2;
-        long total=0;
-    long firstsum=0;
-        for(int i=0;i<nums.length;i++){
+        int firstsum=0;
+        int secondsum=0;
+        int total=0;
+        int count=0;
+        for(int i=0;i<n;i++){
             total+=nums[i];
             if(i<half){
                 firstsum+=nums[i];
             }
         }
-        long secondsum=0;
-        int count=0;
         for(int k=0;k<n;k++){
             secondsum=total-firstsum;
             if(firstsum>secondsum)count++;
             firstsum-=nums[k];
-            firstsum+=(nums[(k+half)%n]);
+            firstsum+=nums[(k+half)%n];
         }
         return count;
     }
