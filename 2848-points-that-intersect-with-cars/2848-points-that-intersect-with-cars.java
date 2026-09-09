722b7b84366e0e2bb1651b18e86e8a7fd70e7a62
Japnoor27 <japnoorkd@gmail.com>
2026-09-09 20:52:01 +0530

    Time: 2 ms (84.64%), Space: 46.6 MB (14.2%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
@@ -1,9 +1,9 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
         int[] diff=new int[102];
-        for(List <Integer> l:nums){
-            int start=l.get(0);
-            int end=l.get(1);
+        for(List<Integer> arr:nums){
+            int start=arr.get(0);
+            int end=arr.get(1);
             diff[start]+=1;
             diff[end+1]-=1;
         }
